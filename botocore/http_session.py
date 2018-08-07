5b4e6f375c5f462258f9c2a97de6ecda63e4caec
Jordan Guymon <jogu.sd@gmail.com>
2018-08-07 16:24:07 -0700

    Make HyperSession optional with better error message

--- botocore/http_session.py
+++ botocore/http_session.py
@@ -15,7 +15,7 @@ from botocore.vendored.six.moves.urllib_parse import unquote
 from botocore.compat import filter_ssl_warnings, urlparse
 from botocore.exceptions import (
     ConnectionClosedError, EndpointConnectionError, HTTPClientError,
-    ReadTimeoutError, ProxyConnectionError
+    ReadTimeoutError, ProxyConnectionError, H2ConfigurationError
 )
 try:
     from urllib3.contrib import pyopenssl
@@ -218,154 +218,156 @@ try:
     from botocore.vendored._hyper.tls import init_context
     from botocore.vendored._hyper.common.util import to_native_string
     from botocore.vendored._hyper.common import exceptions as hyper_errors
-except ImportError:
-    # Hyper is an optional dependency
-    pass
 
-
-class HyperSession(object):
-    """
-    A Requests Transport Adapter that uses hyper to send requests over
-    HTTP/2. This implements some degree of connection pooling to maximise the
-    HTTP/2 gain.
-    """
-    def __init__(self,
-                 verify=True,
-                 proxies=None,
-                 timeout=None,
-                 max_pool_connections=MAX_POOL_CONNECTIONS,
-    ):
-        self._verify = verify
-        self._proxy_config = ProxyConfiguration(proxies=proxies)
-        if timeout is None:
-            timeout = DEFAULT_TIMEOUT
-        self._timeout = timeout
-        self._max_pool_connections = max_pool_connections
-        self._connections = {}
-
-    def _get_connection(self, host, port, scheme, cert=None, verify=True,
-                        proxy=None, timeout=None):
+    class HyperSession(object):
         """
-        Gets an appropriate HTTP/2 connection object based on
-        host/port/scheme/cert tuples.
+        A Requests Transport Adapter that uses hyper to send requests over
+        HTTP/2. This implements some degree of connection pooling to maximise
+        the HTTP/2 gain.
         """
-        secure = (scheme == 'https')
+        def __init__(self,
+                     verify=True,
+                     proxies=None,
+                     timeout=None,
+                     max_pool_connections=MAX_POOL_CONNECTIONS,
+        ):
+            self._verify = verify
+            self._proxy_config = ProxyConfiguration(proxies=proxies)
+            if timeout is None:
+                timeout = DEFAULT_TIMEOUT
+            self._timeout = timeout
+            self._max_pool_connections = max_pool_connections
+            self._connections = {}
 
-        if port is None:  # pragma: no cover
-            port = 80 if not secure else 443
+        def _get_connection(self, host, port, scheme, cert=None, verify=True,
+                            proxy=None, timeout=None):
+            """
+            Gets an appropriate HTTP/2 connection object based on
+            host/port/scheme/cert tuples.
+            """
+            secure = (scheme == 'https')
 
-        if proxy:
-            proxy_headers = self._proxy_config.proxy_headers_for(proxy)
-            proxy_netloc = urlparse(proxy).netloc
-        else:
-            proxy_headers = None
-            proxy_netloc = None
+            if port is None:  # pragma: no cover
+                port = 80 if not secure else 443
 
-        # We put proxy headers in the connection_key, because
-        # ``proxy_headers`` method might be overridden, so we can't
-        # rely on proxy headers being the same for the same proxies.
-        proxy_headers_key = (frozenset(proxy_headers.items())
-                             if proxy_headers else None)
-        connection_key = (host, port, scheme, cert, verify,
-                          proxy_netloc, proxy_headers_key)
-        try:
-            conn = self._connections[connection_key]
-        except KeyError:
-
-            ssl_context = None
-            if not verify:
-                verify = False
-                cert_path = DEFAULT_CA_BUNDLE
-                ssl_context = init_context(cert_path=cert_path, cert=cert)
-                ssl_context.check_hostname = False
-                ssl_context.verify_mode = ssl.CERT_NONE
+            if proxy:
+                proxy_headers = self._proxy_config.proxy_headers_for(proxy)
+                proxy_netloc = urlparse(proxy).netloc
             else:
-                cert_path = get_cert_path(verify)
-                ssl_context = init_context(cert_path=cert_path, cert=cert)
+                proxy_headers = None
+                proxy_netloc = None
 
-            conn = HTTP20Connection(
-                host,
-                port,
-                secure=secure,
-                ssl_context=ssl_context,
-                proxy_host=proxy_netloc,
-                proxy_headers=proxy_headers,
-                timeout=timeout,
-                force_proto='h2',
-            )
-            self._connections[connection_key] = conn
+            # We put proxy headers in the connection_key, because
+            # ``proxy_headers`` method might be overridden, so we can't
+            # rely on proxy headers being the same for the same proxies.
+            proxy_headers_key = (frozenset(proxy_headers.items())
+                                 if proxy_headers else None)
+            connection_key = (host, port, scheme, cert, verify,
+                              proxy_netloc, proxy_headers_key)
+            try:
+                conn = self._connections[connection_key]
+            except KeyError:
 
-        return conn
+                ssl_context = None
+                if not verify:
+                    verify = False
+                    cert_path = DEFAULT_CA_BUNDLE
+                    ssl_context = init_context(cert_path=cert_path, cert=cert)
+                    ssl_context.check_hostname = False
+                    ssl_context.verify_mode = ssl.CERT_NONE
+                else:
+                    cert_path = get_cert_path(verify)
+                    ssl_context = init_context(cert_path=cert_path, cert=cert)
 
-    def send(self, request):
-        """
-        Sends a HTTP message to the server.
-        """
-        try:
-            proxy_url = self._proxy_config.proxy_url_for(request.url)
-            parsed = urlparse(request.url)
-            if isinstance(request.body, str):
-                request.body = request.body.encode('utf-8')
-            conn = self._get_connection(
-                parsed.hostname,
-                parsed.port,
-                parsed.scheme,
-                verify=self._verify,
-                proxy=proxy_url,
-                timeout=self._timeout)
+                conn = HTTP20Connection(
+                    host,
+                    port,
+                    secure=secure,
+                    ssl_context=ssl_context,
+                    proxy_host=proxy_netloc,
+                    proxy_headers=proxy_headers,
+                    timeout=timeout,
+                    force_proto='h2',
+                )
+                self._connections[connection_key] = conn
 
-            # Build the selector.
-            selector = parsed.path
-            selector += '?' + parsed.query if parsed.query else ''
-            selector += '#' + parsed.fragment if parsed.fragment else ''
+            return conn
 
-            conn.request(
-                request.method,
-                selector,
-                request.body,
-                request.headers
-            )
-            hyper_response = conn.get_response()
+        def send(self, request):
+            """
+            Sends a HTTP message to the server.
+            """
+            try:
+                proxy_url = self._proxy_config.proxy_url_for(request.url)
+                parsed = urlparse(request.url)
+                if isinstance(request.body, str):
+                    request.body = request.body.encode('utf-8')
+                conn = self._get_connection(
+                    parsed.hostname,
+                    parsed.port,
+                    parsed.scheme,
+                    verify=self._verify,
+                    proxy=proxy_url,
+                    timeout=self._timeout)
 
-            def ghetto_stream(self, amt=2**16, decode_content=None):
-                while True:
-                    chunk = self.read(amt, decode_content=decode_content)
-                    if not chunk:
-                        break
-                    yield chunk
+                # Build the selector.
+                selector = parsed.path
+                selector += '?' + parsed.query if parsed.query else ''
+                selector += '#' + parsed.fragment if parsed.fragment else ''
 
-            hyper_response.stream = ghetto_stream.__get__(hyper_response)
+                conn.request(
+                    request.method,
+                    selector,
+                    request.body,
+                    request.headers
+                )
+                hyper_response = conn.get_response()
 
-            http_response = botocore.awsrequest.AWSResponse(
-                request.url,
-                hyper_response.status,
-                (map(to_native_string, h) for h in hyper_response.headers.items()),
-                hyper_response
-            )
+                def ghetto_stream(self, amt=2**16, decode_content=None):
+                    while True:
+                        chunk = self.read(amt, decode_content=decode_content)
+                        if not chunk:
+                            break
+                        yield chunk
 
-            if not request.stream_output:
-                # Cause the raw stream to be exhausted immediately. We do it
-                # this way instead of using preload_content because
-                # preload_content will never buffer chunked responses
-                http_response.content
+                hyper_response.stream = ghetto_stream.__get__(hyper_response)
 
-            return http_response
-        except (hyper_errors.NewConnectionError, hyper_errors.ConnectionTimeoutError) as e:
-            raise EndpointConnectionError(endpoint_url=request.url, error=e)
-        except hyper_errors.ProxyError as e:
-            raise ProxyConnectionError(proxy_url=proxy_url, error=e)
-        except hyper_errors.ReadTimeoutError as e:
-            raise ReadTimeoutError(endpoint_url=request.url, error=e)
-        except hyper_errors.ConnectionResetError as e:
-            raise ConnectionClosedError(
-                error=e,
-                request=request,
-                endpoint_url=request.url
-            )
-        except Exception as e:
-            raise HTTPClientError(error=e)
+                http_response = botocore.awsrequest.AWSResponse(
+                    request.url,
+                    hyper_response.status,
+                    (map(to_native_string, h) for h in hyper_response.headers.items()),
+                    hyper_response
+                )
 
-    def close(self):
-        for connection in self._connections.values():
-            connection.close()
-        self._connections.clear()
+                if not request.stream_output:
+                    # Cause the raw stream to be exhausted immediately. We do
+                    # it this way instead of using preload_content because
+                    # preload_content will never buffer chunked responses
+                    http_response.content
+
+                return http_response
+            except (hyper_errors.NewConnectionError, hyper_errors.ConnectionTimeoutError) as e:
+                raise EndpointConnectionError(endpoint_url=request.url, error=e)
+            except hyper_errors.ProxyError as e:
+                raise ProxyConnectionError(proxy_url=proxy_url, error=e)
+            except hyper_errors.ReadTimeoutError as e:
+                raise ReadTimeoutError(endpoint_url=request.url, error=e)
+            except hyper_errors.ConnectionResetError as e:
+                raise ConnectionClosedError(
+                    error=e,
+                    request=request,
+                    endpoint_url=request.url
+                )
+            except Exception as e:
+                raise HTTPClientError(error=e)
+
+        def close(self):
+            for connection in self._connections.values():
+                connection.close()
+            self._connections.clear()
+
+except ImportError:
+    class HyperSession(object):
+        def __init__(self, *args, **kwargs):
+            error_msg = 'Failed to load optional H2 dependencies'
+            raise H2ConfigurationError(error_msg=error_msg)
